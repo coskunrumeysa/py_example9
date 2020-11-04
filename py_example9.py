a5d64de521b112de3b71f166bfece54cf6aa1951
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2020-11-04 14:50:50 +0300

    Add files via upload

---
+++ py_example9.py
@@ -0,0 +1,24 @@
+#Rümeysa Coşkun
+#Pythonda hem metni hemde değişkeni birleştirme basit örnekleri:
+#örnek1
+x = "Rüm"
+print("Dikey"+x)
+#örnek2
+x = 5
+y = 4
+z = x+y
+print(z)
+#örnek3
+x = "Rüm"
+y = "Dikey"
+z = x+y
+print(z)
+#örnek4
+x = 5
+y = 6
+print(x+y)
+#örnek5
+x = "Rüm"
+y = "Dikey"
+print(x+y)
+
